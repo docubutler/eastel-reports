3fa9d6eafe7bc6e5b74889f340693f4a65b8edf1
NUZ\shoaib <shoaib.khalid@gmail.com>
2026-06-24 13:54:43 +0500

    updated the active subscribers query to include the act_usage_unit > 0

--- eastel-report-scripts/reports-pipelines/report-generation-mongo/2-Invoice-Recon/queries/1.js
+++ eastel-report-scripts/reports-pipelines/report-generation-mongo/2-Invoice-Recon/queries/1.js
@@ -10,7 +10,7 @@ Output:
 
 Equivalent PostgreSQL:
 
-SELECT 'Active Subscriber' AS service_type, 'Total Active Subscriber Current' AS charge_type, COUNT(DISTINCT t.msisdn) AS total FROM iot_portal_tb_usage_log t WHERE t.usage_start_time >= '2026-04-01' AND t.usage_start_time < '2026-05-01';
+SELECT 'Active Subscriber' AS service_type, 'Total Active Subscriber Current' AS charge_type, COUNT(DISTINCT t.msisdn) AS total FROM iot_portal_tb_usage_log t WHERE t.usage_start_time >= '2026-04-01' AND t.usage_start_time < '2026-05-01' AND t.act_usage_unit > 0;
 */
 
 db.usage_logs.aggregate([
@@ -19,7 +19,8 @@ db.usage_logs.aggregate([
       usage_start_time: {
         $gte: ISODate("{{start_date}}"),
         $lt: ISODate("{{end_date}}")
-      }
+      },
+      act_usage_unit: { $gt: 0 }
     }
   },
   {
